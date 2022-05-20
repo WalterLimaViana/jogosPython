1c5078a3f0dc2e0a36ac394fed1aae3903301394
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-19 22:57:23 -0300

    jogo de adivinhacao: funcao while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,13 +3,25 @@ print("Bem vindo ao jogo de adivinhação!!")
 print("**********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+while(rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Voce digitou", chute_str)
+    chute = int(chute_str)
 
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-print("Voce digitou", chute_str)
-chute = int(chute_str)
-if(numero_secreto == chute):
-    print("voce acertou!!")
-else:
-    print("Voce errou, continue tentando")
+    if(acertou):
+        print("Voce acertou!!!")
+    else:
+        if(maior):
+            print("Voce errou, o seu chute foi maior que o numero secreto!!!")
+        elif(menor):
+            print("Voce errou. O seu chute foi menor que o numero secreto!!!")
+    rodada = rodada + 1
+print("Fim de jogo")
