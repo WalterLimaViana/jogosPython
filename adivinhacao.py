f3b031dd83fb68cbd071f29e02154119a319e7f5
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-19 21:58:56 -0300

    first class: jogo de adivinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,15 @@
+print("**********************************")
 print("Bem vindo ao jogo de adivinhação!!")
+print("**********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+
+print("Voce digitou", chute_str)
+chute = int(chute_str)
+if(numero_secreto == chute):
+    print("voce acertou!!")
+else:
+    print("Voce errou, continue tentando")
