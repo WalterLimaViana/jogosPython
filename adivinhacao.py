f33ee32c027672b4298efdff5a2528eec95e6667
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-20 21:41:41 -0300

    jogo de adivinhacao: string interpolation

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,11 +6,14 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format( rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite o seu número entre 1 e 100: ")
     print("Voce digitou", chute_str)
     chute = int(chute_str)
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,6 +21,7 @@ while(rodada <= total_de_tentativas):
 
     if(acertou):
         print("Voce acertou!!!")
+        break
     else:
         if(maior):
             print("Voce errou, o seu chute foi maior que o numero secreto!!!")
