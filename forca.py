73eaa719095e1a13923ce8dda1210c61beb265e1
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-22 19:22:00 -0300

    jogo de forca: created while to find a letter in the palavra_secreta

--- forca.py
+++ forca.py
@@ -1,8 +1,24 @@
 def jogar():
     print("**********************************")
-    print("Bem vindo ao jogo da forca!!!")
+    print("***Bem vindo ao jogo da forca!!***")
     print("**********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print(f'A letra {letra} está na posição {index}.')
+                #print("A letra {} está na posição {}.".format(letra, index))
+            index = index + 1
+        print("jogando...")
 
     print("Fim de jogo")
 if(__name__ == "__main__"):
