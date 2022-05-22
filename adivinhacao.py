50f4f29c42b0da6398e47128f542e495a9b66f58
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-21 23:22:32 -0300

    jogo de adivinhacao: created difficult level using input

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,8 +5,20 @@ print("Bem vindo ao jogo de adivinhação!!")
 print("**********************************")
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Intermediário (3) Díficil")
+
+nivel = int(input("Defina o nivel: "))
+
+if(nivel == 1):
+    total_de_tentativas = 15
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range (1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format( rodada, total_de_tentativas))
